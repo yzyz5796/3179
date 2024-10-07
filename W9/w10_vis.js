f01058d0fadf0068e797185b1c8242906eb5c2d6
yzyz5796 <ymak0003@student.monash.edu>
2024-10-07 19:08:01 +0800

    Add files via upload

--- W9/w10_vis.js
+++ W9/w10_vis.js
@@ -1,5 +1,5 @@
-var vg_1 = "map.json";
-var vg_2 = "linegraph.json";
+var vg_1 = "map.vg.json";
+var vg_2 = "linegraph.vg.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
