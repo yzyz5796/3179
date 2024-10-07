4f5a6390fe5de0ea819f3f2472f2e66f558e525c
yzyz5796 <ymak0003@student.monash.edu>
2024-10-07 18:30:45 +0800

    Create w10_vis.js

---
+++ W9/w10_vis.js
@@ -0,0 +1,10 @@
+var vg_1 = "map.json";
+var vg_2 = "linegraph.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#linegraph", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
