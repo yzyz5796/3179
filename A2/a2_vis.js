c4dff096f58ef393fe0f2d592a86ea7949758a64
yzyz5796 <ymak0003@student.monash.edu>
2024-10-11 05:01:18 +0800

    Add files via upload

---
+++ A2/a2_vis.js
@@ -0,0 +1,25 @@
+var vg_1 = "map.vg.json";
+var vg_2 = "piechart.vg.json";
+var vg_3 = "barchart.vg.json";
+var vg_4 = "linegraph.vg.json";
+var vg_5 = "bubbleplot.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#piechart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#barchart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#linegraph", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bubbleplot", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
